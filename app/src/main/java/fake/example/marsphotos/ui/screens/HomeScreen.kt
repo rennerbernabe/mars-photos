3fb94f46d4c2927238e38476b55fdebaa406364c
rennerbernabe_ <renner.bernabe@cartrack.com>
2024-01-31 09:54:58 +0800

    Add retry action

--- app/src/main/java/fake/example/marsphotos/ui/screens/HomeScreen.kt
+++ app/src/main/java/fake/example/marsphotos/ui/screens/HomeScreen.kt
@@ -28,6 +28,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
@@ -49,11 +50,12 @@ import fake.example.marsphotos.ui.theme.MarsPhotosTheme
 @Composable
 fun HomeScreen(
     marsUiState: MarsUiState,
+    retryAction: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     when (marsUiState) {
         is MarsUiState.Success -> PhotosGridScreen(marsUiState.photos, modifier)
-        MarsUiState.Error -> ErrorScreen(modifier = modifier.fillMaxSize())
+        MarsUiState.Error -> ErrorScreen(retryAction, modifier = modifier.fillMaxSize())
         MarsUiState.Loading -> LoadingScreen(modifier = modifier.fillMaxSize())
     }
 }
@@ -68,7 +70,7 @@ fun LoadingScreen(modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun ErrorScreen(modifier: Modifier = Modifier) {
+fun ErrorScreen(retryAction: () -> Unit, modifier: Modifier = Modifier) {
     Column(
         modifier = modifier,
         verticalArrangement = Arrangement.Center,
@@ -78,6 +80,9 @@ fun ErrorScreen(modifier: Modifier = Modifier) {
             painter = painterResource(id = R.drawable.ic_connection_error), contentDescription = ""
         )
         Text(text = stringResource(R.string.loading_failed), modifier = Modifier.padding(16.dp))
+        Button(onClick = retryAction) {
+            Text(stringResource(R.string.retry))
+        }
     }
 }
 
